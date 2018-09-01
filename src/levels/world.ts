2f4cc5e82209fd7d13de2b943edb967a5c2401f3
Matt McKenna <matt@mtmckenna.com>
2018-09-01 10:09:58 -0500

    Handle AudioContext on Safari

--- src/levels/world.ts
+++ src/levels/world.ts
@@ -123,6 +123,8 @@ export default class World extends Level {
     this.game.player.move(this.playerSpawnPosition, false);
     this.game.player.setConvoMode(false);
 
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+
     const ac = new AudioContext();
     const tempo = 120;
     const sequence = new TinyMusic.Sequence(ac, tempo, [
