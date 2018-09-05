3dd733422ea66fba90d9490efc2ac421961071e1
Matt McKenna <matt@mtmckenna.com>
2018-09-04 19:57:45 -0700

    moving private methods into separate functions so they can be mangled... getting ugly

--- src/levels/world.ts
+++ src/levels/world.ts
@@ -24,10 +24,6 @@ const TEXT_INTROS = [
   ["talk to them", "about their", "interests!"],
 ];
 
-// const LISTEN_WORDS = [
-//   ["well done! you", "had a convo!"],
-// ];
-
 const WB_START_POS = { x: TS * 18, y: TS * 7 };
 
 export default class World extends Level {
@@ -80,19 +76,19 @@ export default class World extends Level {
     this.energyBar = new EnergyBar(this.game, { x: 0, y: game.squareSize }, "ENERGY");
 
     this.box = new Box(this.game, this.game.boxPos, this.game.boxSize);
-    this.createBuddies();
+    createBuddies.call(this);
   }
 
   public handleInput(key) {
     if (this.game.transitioning) return;
-    if (this.handleBoxInput()) return;
+    if (handleBoxInput.call(this)) return;
     this.inputBuffer = { pressedAt: this.game.timestamp, key };
   }
 
   public handleTouch(touch) {
     if (this.game.transitioning) return;
     if (this.game.player.walking) return;
-    if (this.handleBoxInput()) return;
+    if (handleBoxInput.call(this)) return;
 
     const { camera } = this.game;
     const { height, width } = camera.size;
@@ -110,22 +106,22 @@ export default class World extends Level {
     if (absHorizontalDistance < minMoveTheshold && absVerticalDistance < minMoveTheshold) return;
 
     if (absHorizontalDistance > absVerticalDistance) {
-      this.movePlayerHorizontally(horizontalDistance);
+      movePlayerHorizontally.call(this, horizontalDistance);
     } else {
-      this.movePlayerVertically(verticalDistance);
+      movePlayerVertically.call(this, verticalDistance);
     }
   }
 
   public resize() {
     const energyBarX = Math.floor((this.game.canvas.width - this.energyBar.drawingSize.width) / 2);
     this.energyBar.move({ x: energyBarX, y: this.energyBar.pos.y });
-    this.updateBox();
+    updateBox.call(this);
   }
 
   public update(timestamp) {
     super.update(timestamp);
-    this.processInput();
-    this.startConvo(this.game.player.tileIndex);
+    processInput.call(this);
+    startConvo.call(this, this.game.player.tileIndex);
     this.game.camera.moveToPlayer(this.game.player);
 
     // Make walking buddy walk
@@ -154,9 +150,9 @@ export default class World extends Level {
     this.walkingBuddy.move(WB_START_POS);
     this.game.player.move(this.playerSpawnPosition);
     this.game.player.setConvoMode(false);
-    this.showNextIntroBox();
-    this.learnFromConvo();
-    this.showListenBox();
+    showNextIntroBox.call(this);
+    learnFromConvo.call(this);
+    showListenBox.call(this);
 
     // @ts-ignore
     const NormalizedAudioContext = window.AudioContext || webkitAudioContext;
@@ -172,193 +168,193 @@ export default class World extends Level {
     sequence.loop = false;
     sequence.play();
   }
+}
 
-  private createBuddies() {
-    this.listenBuddy = new Buddy(this.game);
-    this.listenBuddy.move({ x: TS * 7, y: TS * 10 });
-    this.listenBuddy.skills.push(LISTEN);
+function handleBoxInput(): boolean {
+  if (this.state === "play") return false;
 
-    const pastryBuddy = new Buddy(this.game);
-    pastryBuddy.move({ x: TS * 8, y: TS * 1 });
-    pastryBuddy.skills.push("pastries");
-
-    const travelBuddy = new Buddy(this.game);
-    travelBuddy.move({ x: TS * 18, y: TS * 3 });
-    travelBuddy.skills.push("france");
-
-    const sportsBuddy = new Buddy(this.game);
-    sportsBuddy.move({ x: TS * 25, y: TS * 2 });
-    sportsBuddy.skills.push("sports");
-
-    this.specialBuddy = new Buddy(this.game);
-    this.specialBuddy.move({ x: TS * 23, y: TS * 18 });
-    this.specialBuddy.look("left");
-    this.specialBuddy.skills.push("anime");
-
-    const booksBuddy = new Buddy(this.game);
-    booksBuddy.move({ x: TS * 15, y: TS * 18 });
-    booksBuddy.skills.push("books");
-    booksBuddy.look("up");
-
-    const mathBuddy = new Buddy(this.game);
-    mathBuddy.move({ x: TS * 26, y: TS * 8 });
-    mathBuddy.skills.push("math");
-    mathBuddy.look("left");
-
-    this.walkingBuddy = new Buddy(this.game);
-    this.walkingBuddy.autoWalkDirection = "left";
-    this.walkingBuddy.skills.push("math");
-    this.walkingBuddy.animations.walking.duration = 750;
-
-    this.buddies = [
-      this.listenBuddy,
-      pastryBuddy,
-      travelBuddy,
-      sportsBuddy,
-      this.specialBuddy,
-      booksBuddy,
-      mathBuddy,
-      this.walkingBuddy,
-    ];
+  // Sleeping
+  if (this.state === "sleeping" || this.state === "post-convo") {
+    this.state = "play";
+    this.box.visible = false;
   }
 
-  private handleBoxInput(): boolean {
-    if (this.state === "play") return false;
+  if (this.state === "sleeping") this.handleInput("ArrowDown");
+  if (this.state === "post-convo" && !this.learnedListen) {
+    showListenBox.call(this);
+    this.learnedListen = true;
+  }
 
-    // Sleeping
-    if (this.state === "sleeping" || this.state === "post-convo") {
-      this.state = "play";
-      this.box.visible = false;
-    }
+  // Run through intro
+  if (this.state === "intro") showNextIntroBox.call(this);
 
-    if (this.state === "sleeping") this.handleInput("ArrowDown");
-    if (this.state === "post-convo" && !this.learnedListen) {
-      this.showListenBox();
-      this.learnedListen = true;
-    }
+  return true;
+}
 
-    // Run through intro
-    if (this.state === "intro") this.showNextIntroBox();
+function showNextIntroBox() {
+  if (this.state !== "intro") return; // TODO: remove this debug code!
+  const words = this.textIntros[0];
+  if (!words) {
+    this.state = "play";
+    this.box.visible = false;
+    return;
+  }
 
+  this.box.visible = true;
+  this.box.setWords(words);
+  this.box.animateTextIn(this.game.timestamp);
+  this.textIntros.shift();
+}
+
+function learnFromConvo() {
+  if (!this.currentBuddy) return;
+
+  const skill = this.currentBuddy.skills[randomIndexFromArray(this.currentBuddy.skills)];
+  this.box.visible = true;
+
+  if (this.game.player.skills.includes(skill)) {
+    this.box.setWords(["nice convo!", "that was a", "good time"]);
+  } else {
+    this.box.setWords(["nice convo!", "you learned", `${skill}!`]);
+    this.game.player.skills.push(skill);
+  }
+
+  this.box.animateTextIn(this.game.timestamp);
+}
+
+function showListenBox() {
+  if (this.currentBuddy !== this.listenBuddy) return;
+  this.listenBuddy.walk("right");
+}
+
+function updateBox() {
+  this.box.move(this.game.boxPos);
+  this.box.updateSize(this.game.boxSize);
+}
+
+function startConvo(tileIndex: IPoint): boolean {
+  // Check if we're overlapping interactables like buddies
+  const overlappedInteractable = this.interactables.find((interactable) => {
+    return interactable.tileIndex.x === tileIndex.x &&
+      interactable.tileIndex.y === tileIndex.y;
+  });
+
+  if (overlappedInteractable) {
+    this.currentBuddy = overlappedInteractable as Buddy;
+    this.playerSpawnPosition.x = this.game.player.pos.x;
+    this.playerSpawnPosition.y = this.game.player.pos.y;
+    this.game.queueNextLevel(this.game.levels.convo);
     return true;
   }
 
-  private showNextIntroBox() {
-    if (this.state !== "intro") return; // TODO: remove this debug code!
-    const words = this.textIntros[0];
-    if (!words) {
-      this.state = "play";
-      this.box.visible = false;
-      return;
-    }
+  return false;
+}
 
-    this.box.visible = true;
-    this.box.setWords(words);
-    this.box.animateTextIn(this.game.timestamp);
-    this.textIntros.shift();
+function sleep() {
+  this.game.player.energy = 1;
+  this.energyBar.animateToLevel(this.game.player.energy);
+  this.box.setWords(["", "zzzzzz...", ""]);
+  this.box.animateTextIn(this.game.timestamp);
+  this.box.visible = true;
+  this.state = "sleeping";
+}
+
+function movePlayerVertically(touchDistance: number) {
+  if (touchDistance < 0) this.handleInput("ArrowUp");
+  if (touchDistance > 0) this.handleInput("ArrowDown");
+}
+
+function movePlayerHorizontally(touchDistance: number) {
+  if (touchDistance > 0) this.handleInput("ArrowRight");
+  if (touchDistance < 0) this.handleInput("ArrowLeft");
+}
+
+function createBuddies() {
+  this.listenBuddy = new Buddy(this.game);
+  this.listenBuddy.move({ x: TS * 7, y: TS * 10 });
+  this.listenBuddy.skills.push(LISTEN);
+
+  const pastryBuddy = new Buddy(this.game);
+  pastryBuddy.move({ x: TS * 8, y: TS * 1 });
+  pastryBuddy.skills.push("pastries");
+
+  const travelBuddy = new Buddy(this.game);
+  travelBuddy.move({ x: TS * 18, y: TS * 3 });
+  travelBuddy.skills.push("france");
+
+  const sportsBuddy = new Buddy(this.game);
+  sportsBuddy.move({ x: TS * 25, y: TS * 2 });
+  sportsBuddy.skills.push("sports");
+
+  this.specialBuddy = new Buddy(this.game);
+  this.specialBuddy.move({ x: TS * 23, y: TS * 18 });
+  this.specialBuddy.look("left");
+  this.specialBuddy.skills.push("anime");
+
+  const booksBuddy = new Buddy(this.game);
+  booksBuddy.move({ x: TS * 15, y: TS * 18 });
+  booksBuddy.skills.push("books");
+  booksBuddy.look("up");
+
+  const mathBuddy = new Buddy(this.game);
+  mathBuddy.move({ x: TS * 26, y: TS * 8 });
+  mathBuddy.skills.push("math");
+  mathBuddy.look("left");
+
+  this.walkingBuddy = new Buddy(this.game);
+  this.walkingBuddy.autoWalkDirection = "left";
+  this.walkingBuddy.skills.push("math");
+  this.walkingBuddy.animations.walking.duration = 750;
+
+  this.buddies = [
+    this.listenBuddy,
+    pastryBuddy,
+    travelBuddy,
+    sportsBuddy,
+    this.specialBuddy,
+    booksBuddy,
+    mathBuddy,
+    this.walkingBuddy,
+  ];
+}
+
+// This function is gigantic not because I'm a bad person but because
+// I needed to de-dupe the switch statements to save space.
+function processInput(): boolean {
+  const timeSinceInput = this.game.timestamp - this.inputBuffer.pressedAt;
+  if (timeSinceInput > 50) return false; // For input buffering
+  if (this.game.player.walking) return false;
+
+  // Get the tile index that we'd be walking onto
+  const tileIndex = Object.assign({}, this.game.player.tileIndex);
+  let direction = null;
+
+  switch (this.inputBuffer.key) {
+    case "ArrowUp":
+      direction = "up";
+      tileIndex.y -= 1;
+      break;
+    case "ArrowDown":
+      direction = "down";
+      tileIndex.y += 1;
+      break;
+    case "ArrowLeft":
+      direction = "left";
+      tileIndex.x -= 1;
+      break;
+    case "ArrowRight":
+      direction = "right";
+      tileIndex.x += 1;
+      break;
   }
 
-  private learnFromConvo() {
-    if (!this.currentBuddy) return;
+  // Start a convo if overlapping a buddy
+  if (startConvo.call(this, tileIndex)) return;
 
-    const skill = this.currentBuddy.skills[randomIndexFromArray(this.currentBuddy.skills)];
-    this.box.visible = true;
+  // Check if we're overlapping the door
+  if (this.tileAtIndex(tileIndex).interactable) this.sleep();
 
-    if (this.game.player.skills.includes(skill)) {
-      this.box.setWords(["nice convo!", "that was a", "good time"]);
-    } else {
-      this.box.setWords(["nice convo!", "you learned", `${skill}!`]);
-      this.game.player.skills.push(skill);
-    }
-
-    this.box.animateTextIn(this.game.timestamp);
-  }
-
-  private showListenBox() {
-    if (this.currentBuddy !== this.listenBuddy) return;
-    this.listenBuddy.walk("right");
-  }
-
-  private updateBox() {
-    this.box.move(this.game.boxPos);
-    this.box.updateSize(this.game.boxSize);
-  }
-
-  // This function is gigantic not because I'm a bad person but because
-  // I needed to de-dupe the switch statements to save space.
-  private processInput(): boolean {
-    const timeSinceInput = this.game.timestamp - this.inputBuffer.pressedAt;
-    if (timeSinceInput > 50) return false; // For input buffering
-    if (this.game.player.walking) return false;
-
-    // Get the tile index that we'd be walking onto
-    const tileIndex = Object.assign({}, this.game.player.tileIndex);
-    let direction = null;
-
-    switch (this.inputBuffer.key) {
-      case "ArrowUp":
-        direction = "up";
-        tileIndex.y -= 1;
-        break;
-      case "ArrowDown":
-        direction = "down";
-        tileIndex.y += 1;
-        break;
-      case "ArrowLeft":
-        direction = "left";
-        tileIndex.x -= 1;
-        break;
-      case "ArrowRight":
-        direction = "right";
-        tileIndex.x += 1;
-        break;
-    }
-
-    // Start a convo if overlapping a buddy
-    if (this.startConvo(tileIndex)) return;
-
-    // Check if we're overlapping the door
-    if (this.tileAtIndex(tileIndex).interactable) this.sleep();
-
-    // If we're not overlapping anything fun, just walk
-    this.game.player.walk(direction);
-    return true;
-  }
-
-  private startConvo(tileIndex: IPoint): boolean {
-    // Check if we're overlapping interactables like buddies
-    const overlappedInteractable = this.interactables.find((interactable) => {
-      return interactable.tileIndex.x === tileIndex.x &&
-        interactable.tileIndex.y === tileIndex.y;
-    });
-
-    if (overlappedInteractable) {
-      this.currentBuddy = overlappedInteractable as Buddy;
-      this.playerSpawnPosition.x = this.game.player.pos.x;
-      this.playerSpawnPosition.y = this.game.player.pos.y;
-      this.game.queueNextLevel(this.game.levels.convo);
-      return true;
-    }
-
-    return false;
-  }
-
-  private sleep() {
-    this.game.player.energy = 1;
-    this.energyBar.animateToLevel(this.game.player.energy);
-    this.box.setWords(["", "zzzzzz...", ""]);
-    this.box.animateTextIn(this.game.timestamp);
-    this.box.visible = true;
-    this.state = "sleeping";
-  }
-
-  private movePlayerVertically(touchDistance: number) {
-    if (touchDistance < 0) this.handleInput("ArrowUp");
-    if (touchDistance > 0) this.handleInput("ArrowDown");
-  }
-
-  private movePlayerHorizontally(touchDistance: number) {
-    if (touchDistance > 0) this.handleInput("ArrowRight");
-    if (touchDistance < 0) this.handleInput("ArrowLeft");
-  }
+  // If we're not overlapping anything fun, just walk
+  this.game.player.walk(direction);
+  return true;
 }
